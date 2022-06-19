f22f378a9940304ebf2539d1e1d97ba873d8a4e7
Blaise <comoblaise@gmail.com>
2022-06-18 23:18:05 -0600

    Insertions

---
+++ main_Example_1.py
@@ -0,0 +1,23 @@
+import bs4
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
+
+# You can also import request and use those methods to pull data from the internet
+
+my_url = 'https://www.newegg.com/p/pl?d=graphics+cards'
+
+# This handy piece of code downloads the web page from my_url 
+uClient = uReq(my_url)
+
+# Saves the html file as page_html
+page_html = uClient.read()
+
+uClient.close()
+
+# This parses page_html
+page_soup = BeautifulSoup(page_html, "html.parser")
+
+# Grabs all data in each container, this is sort of like a list, you can call len on it to see how many item-containers it found
+# This would be a good spot to use regex and a for loop to try and catch common classes
+containers = page_soup.findAll("div", {"class":"item-container"})
+
